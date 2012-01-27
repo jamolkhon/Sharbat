d120719f928b68b2e8a20a07ebc22498644e050f
Jamolkhon Khakimov <jkhakimov@gmail.com>
2012-01-27 15:21:26 +0500

    Added getFields method

--- src/Sharbat/Reflect/ObjectUtils.php
+++ src/Sharbat/Reflect/ObjectUtils.php
@@ -19,11 +19,26 @@ class ObjectUtils {
   }
 
   public function getFieldValueMap($object) {
+    $fieldValueMap = array();
+    $fields = $this->getFields(get_class($object));
+
+    foreach ($fields as $field) {
+      $getterMethodName = 'get' . ucfirst($field);;
+
+      if (method_exists($object, $getterMethodName)) {
+        $fieldValueMap[$field] = $object->$getterMethodName();
+      }
+    }
+
+    return $fieldValueMap;
+  }
+
+  public function getFields($qualifiedClassName) {
     $fields = array();
 
-    foreach (get_class_methods($object) as $methodName) {
-      if (substr($methodName, 0, 3) === 'get') {
-        $fields[lcfirst(substr($methodName, 3))] = $object->$methodName();
+    foreach (get_class_methods($qualifiedClassName) as $methodName) {
+      if (strlen($methodName) > 3 && substr($methodName, 0, 3) === 'get') {
+        $fields[] = lcfirst(substr($methodName, 3));
       }
     }
 
